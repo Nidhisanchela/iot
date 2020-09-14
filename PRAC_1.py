c77a7a5683472d7ebb0c2041fa652e3e235e0f7b
Nidhisanchela <71246788+Nidhisanchela@users.noreply.github.com>
2020-09-14 18:28:31 +0530

    Create PRAC_1.py

---
+++ PRAC_1.py
@@ -0,0 +1,14 @@
+import telepot
+token = '1386540790:AAE1MurcINL6ZK8uXDaAHeRka2i_sMyBNA4'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+
+Nidhi chat I'd:1323900616
+
+https://api.telegram.org/bot1386540790:AAE1MurcINL6ZK8uXDaAHeRka2i_sMyBNA4/getUpdates
+
+https://api.telegram.org/bot1386540790:AAE1MurcINL6ZK8uXDaAHeRka2i_sMyBNA4/getme
+
+Urvashi I'd:850363090
+
+https://api.telegram.org/bot1386540790:AAE1MurcINL6ZK8uXDaAHeRka2i_sMyBNA4/sendMessage?chat_id=850363090&text=Hello
